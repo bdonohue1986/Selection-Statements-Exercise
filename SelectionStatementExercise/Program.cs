a4f39db6ba5c94e2f18f0ea8553fbdac6c4e04ed
bdonohue1986 <100311620+bdonohue1986@users.noreply.github.com>
2022-09-28 11:07:14 -0400

    inital commit

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int G = 700;
+            Console.WriteLine("Can you guess my favorite number bewtween 1 and 1000?");
+            int userInput = int.Parse(Console.ReadLine());
+            if (userInput < G)
+            {
+                Console.WriteLine("TO Low!!");
+            }
+            else if (userInput > 1000)
+            {
+                Console.WriteLine("Nevermind");
+            }
+            else
+            {
+                Console.WriteLine("TO HIGH!");
+            }
         }
     }
 }
